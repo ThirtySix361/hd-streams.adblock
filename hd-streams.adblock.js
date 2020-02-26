eb62b5c7510c7d61449f198d282ec6e93110039f
ThirtySix <boy36@hotmail.de>
2020-02-26 02:41:41 +0100

    (v1.2) added auto-height-adjust

--- hd-streams.adblock.js
+++ hd-streams.adblock.js
@@ -11,15 +11,28 @@ setInterval(function(e) {
                 let daddy = e.parentNode;
                 let src = e.src;
                 let btn = document.createElement("div");
-                let height = window.innerHeight / 100 * 80;
                 btn.innerHTML = '<div style="text-align: center; color: #3279a8; font-size: 24px;">Ad\'s removed by ThirtySix</div>';
-                btn.innerHTML += '<iframe allowfullscreen id="36" style="width: 100%; height: '+height+'px;" sandbox="allow-scripts" src="'+src+'"></iframe>';
+                btn.innerHTML += '<iframe allowfullscreen id="36" style="width: 100%;" sandbox="allow-scripts" src="'+src+'"></iframe>';
                 btn.style = "";
                 btn.href = src;
                 daddy.innerHTML = "";
                 daddy.appendChild(btn);
-                setTimeout(function(x){console.log("ads removed by 36");},1000);
+                setTimeout(function(x){
+                    document.querySelector('iframe[id="36"]').height = window.innerHeight * 0.75;
+                    console.log("ads removed by 36");
+                },1000);
+
             } 
         })
     }     
 }, 1000);
+
+window.addEventListener('resize', function() {
+    if ( document.querySelectorAll("iframe").length != 0 ) {
+        document.querySelectorAll("iframe").forEach(function(e){
+            if ( e.id == "36" ) {
+                e.height = window.innerHeight * 0.75;
+            }
+        }) 
+    }
+});
